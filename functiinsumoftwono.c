842d80918f045b3adc32cd6f446f1714e26dd196
sahithi0511 <srisahithich@gmail.com>
2020-08-16 12:11:27 +0530

    some description about return

--- functiinsumoftwono.c
+++ functiinsumoftwono.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int sum_of_two_numbers(int a,int b)//function definition
 {
-	return a+b;
+	return a+b;//when return raises it immediately goes to return it will not check the next line
+	return 100;
+
+
 }
 int main()//Execution starts from main function irrespeccctive of any functions it start with main function
 {
