a80923b71083f5f122ba5ae1d454ff26c6db1903
sahithi0511 <srisahithich@gmail.com>
2020-08-21 21:51:17 +0530

    made some trails

--- fingercount.c
+++ fingercount.c
@@ -6,24 +6,32 @@ for(i=1;i<=n;i++)
 {
 	if(j>k && k!=0)
 	{
+	
 	k=k-1;
+	if(k==0)
+	{
+		
+		printf("%d is in forward direction \n",i);
+		printf("%d %d \n",j,k);
+	}
 	if(k>0)
 	{
-	 j--;
+	j--;
 	 printf("%d is in backward direction \n",i);
 	 
 	}
 }
     else  if(k==0)
 	{
-	printf("%d is in forward direction \n",i);		
+     printf("%d is in forward direction \n",i);		
 	   k=4;  
 	
 	}
 	else
 	{
-		printf("%d is in forward direction \n",i);	
-       	j=j+1;
+		printf("%d is in forward direction \n",i);
+		j=j+1;
+		//printf("j is %d",j);
 	}
 }
 }
