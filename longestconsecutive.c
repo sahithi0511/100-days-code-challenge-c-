4f4b3aa057bd487f90bcb4cabc11d01e92783912
sahithi0511 <srisahithich@gmail.com>
2020-09-16 17:16:56 +0530

    longestconsecutive

---
+++ longestconsecutive.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void longest_consecutive(int n)
+{
+	int r,sum=0,big=0;
+	while(n)
+	{
+		r=n&1;
+		if(r==1)
+		{
+			sum=sum+1;
+			n=n>>1;
+			if(n&1==1)
+			{
+			  sum=sum+1;
+			 // printf(" sum is %d \n",sum);
+			  n=n>>1;
+			if(big<sum)
+			{ 
+			  big=sum;
+				
+			}	
+			}
+			else
+			{
+				//big=sum;
+				printf(" big is %d \n",big+1);
+				sum=0;
+				continue;
+			}
+		
+			
+		}
+		else{
+			n=n>>1;
+			
+		}
+		}
+	//	printf("%d",big);
+	}
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	longest_consecutive(n);
+}
