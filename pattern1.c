daef81e1086415b20916deb77d99cee1db20352e
sahithi0511 <srisahithich@gmail.com>
2020-09-21 16:42:28 +0530

    pattern1

---
+++ pattern1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void pattern(int n)
+{
+	int i,j;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	pattern(n);
+}
