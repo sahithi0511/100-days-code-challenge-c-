d8614ed9a00acf628ef8077549790b2a943808c0
sahithi0511 <srisahithich@gmail.com>
2020-09-22 23:27:36 +0530

    pattern reverse

---
+++ patternrev.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int pattern_rev(int n)
+{
+	
+	int i,j,k,num=1,f=0;
+	for(i=1;i<=n;i++)
+	{
+		for(k=1;k<=(n-i);k++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=((i*2)-1);j++)
+		{
+			
+			printf("%d",i);
+		     // num=i+2;
+			 
+		  
+		}
+		printf("\n");
+	}
+}
+		
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	pattern_rev(n);
+}
+//    1
+//   222
+//  33333
+// 4444444
+//555555555
