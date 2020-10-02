354b01622ac3fced46bcef595ffb898c477c103d
sahithi0511 <srisahithich@gmail.com>
2020-10-02 15:54:55 +0530

    even odd array

--- evenoddarray.c
+++ evenoddarray.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int * sum_arr(int *arr,int n,int *sum)
+int * sum_arr(int *arr,int n)
 {
-	int i,s=0,a[100];
+	int i,s=0,a[100],sum=0;
 	for(i=0;i<n;i++)
 	{
 		if(arr[i]%2==0)
@@ -17,21 +17,28 @@ int * sum_arr(int *arr,int n,int *sum)
 		
 		
 	}
+	a[0]=sum;
+	a[1]=s;
+	return a;
 
      	
-	return s;
+//	return s;
 }
 int main()
 {
-	int n,arr[100],*b,i,sum[1];
+	int n,arr[100],*b,i;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&arr[i]);
 	}
-	b=sum_arr(arr,n,sum);
-    printf("%d",b);
-    printf("%d",sum);
+	b=sum_arr(arr,n);
+
+	
+		printf("%d ",b[0]);
+		printf("%d",b[1]);
+	
+    
 	
 
 }
