b5e861a58057043d4739234234faf1134ee95962
sahithi0511 <srisahithich@gmail.com>
2020-10-08 16:26:21 +0530

    marks of students

---
+++ hackerrankmarksofstudents.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  int i,c=0;
+  for(i=0;i<number_of_students;i++)
+  {
+      if(i%2==0 && gender=='b')
+      {
+             c=c+marks[i];
+      }
+      else if(i%2!=0 && gender=='g')
+      {
+          c=c+marks[i];
+          
+      }
+  }
+  return c;
+}
+
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+    int student;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
