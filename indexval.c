9436d3624581064639cc017be69e223589931984
sahithi0511 <srisahithich@gmail.com>
2020-10-13 20:55:03 +0530

    index value array

--- indexval.c
+++ indexval.c
@@ -1,32 +1,35 @@
 #include<stdio.h>
 void index_val(int *num,int *index,int n)
 {
-	int i=0,a[100]={-1},d=0,k,e,f;
+	int i,j,k,a[100],d=0,e,f;
 	for(i=0;i<n;i++)
 	{
 		a[i]=-1;
 	}
-	
-	for(i=n-1;i>=0;i--)
-	{
-		if(a[index[i]]==-1)
-		{
-			printf("%d is a[index[i]]\n",a[index[i]]);
+    for(i=0,j=0;i<n,j<n;i++,j++)
+    {
+    
+    		if(a[index[j]]==-1)
+    		{
+    			a[index[j]]=num[i];
+    			//printf("%d\n",a[index[j]]);
+			}
+			else
+			{
+				for(k=n-1;k>=index[j];k--)
+				{
+					a[k+1]=a[k];
+				}
 			
-	        a[index[i]]=num[i];
-		}
-		else
-		{
-	           d=index[i];
-	           printf("%d\n",d);
-	           
-		}
+				a[index[j]]=num[i];
+				
+			}
 	}
 	for(i=0;i<n;i++)
 	{
-		printf("%d",a[index[i]]);
+		printf("%d ",a[i]);
 	}
-	 
+
 }
 int main()
 {
